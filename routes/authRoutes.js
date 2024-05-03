0c98425877d1f93961d75787c14a0b1925768123
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-05-03 09:57:49 +0200

    finish mongoddb auth shell

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,19 @@
 /* Routes for Auth */
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+//connect to mongodb
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log("Connected to MongoDb");
+}).catch((error) => {
+    console.error("Error connecting to database...");
+});
+
+//User model
+const User = require("../models/User");
 
 //Add a new user
 router.post("/register", async (req, res) => {
@@ -14,6 +26,9 @@ router.post("/register", async (req, res) => {
         }
 
         // Correct - save user
+        const user = new User({username, password});
+        await user.save();
+
         res.status(201).json({ message: "User created" });
 
     } catch (error) {
@@ -32,14 +47,18 @@ router.post("/login", async (req, res) => {
         }
 
         // check credentials
-        if (username === "Jonas" && password === "password") {
-            res.status(200).json({ message: "Login successful" })
-        } else {
-            res.status(401).json({ error: "Invalid username/password" });
+        const user = await User.findOne({username});
+        if(!user) {
+            return res.status(401).json({erro: "Incorrect username/password!"});
         }
 
-        // Correct - save user
-        res.status(201).json({ message: "User created" });
+        //check password
+        const isPasswordMatch = await user.comparePassword(password);
+        if(!isPasswordMatch) {
+            return res.status(401).json({erro: "Incorrect username/password!"});
+        } else {
+            res.status(200).json({message: "User logged in!"});
+        }
     } catch (error) {
         res.status(500).json({ error: "Server error" });
     }
